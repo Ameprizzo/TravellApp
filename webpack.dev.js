7caa523c95a47c2d610af3f604376e1f374e9b4a
Ameprizzo <amedeus926@gmail.com>
2020-03-11 23:26:05 +0300

    Adding webpack config

---
+++ webpack.dev.js
@@ -0,0 +1,60 @@
+const path = require("path")
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            }, {
+                test: /\.(jpg|png|svg|jpg|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new BundleAnalyzerPlugin(),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+
+}
